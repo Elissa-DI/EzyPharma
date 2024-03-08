19ca99a9a8c02f82ce395539bd5d5693deb6a702
Elissa-DI <elissadusabe@gmail.com>
2024-03-08 11:38:53 +0200

    working on articles

--- app/patient/home/(tabs)/_layout.tsx
+++ app/patient/home/(tabs)/_layout.tsx
@@ -6,27 +6,39 @@ import { FontAwesome5, Ionicons, MaterialCommunityIcons } from '@expo/vector-ico
 const TabsLayout = () => {
   return (
     <Tabs screenOptions={{
-      tabBarInactiveTintColor: Colors.primary,
+      tabBarInactiveTintColor: Colors.grey,
     }}>
-      <Tabs.Screen name="landingPage" options={{
-        tabBarLabel: 'Home',
-        tabBarIcon: ({ color, size }) => <Ionicons name="home" color={color} size={size} />,
-      }}
+      <Tabs.Screen
+        name="landingPage"
+        options={{
+          tabBarLabel: 'Home',
+          tabBarIcon: ({ color, size }) => <Ionicons name="home" color={color} size={size} />,
+          headerShown: false,
+        }}
       />
-      <Tabs.Screen name="searchPharmacy" options={{
-        tabBarLabel: 'Search',
-        tabBarIcon: ({ color, size }) => <Ionicons name="search" color={color} size={size} />,
-      }}
+      <Tabs.Screen
+        name="searchPharmacy"
+        options={{
+          tabBarLabel: 'Search',
+          tabBarIcon: ({ color, size }) => <Ionicons name="search" color={color} size={size} />,
+          headerShown: false,
+        }}
       />
-      <Tabs.Screen name="notificationPage" options={{
-        tabBarLabel: 'Trips',
-        tabBarIcon: ({ color, size }) => <FontAwesome5 name="bell" color={color} size={size} />,
-      }}
+      <Tabs.Screen
+        name="notificationPage"
+        options={{
+          tabBarLabel: 'Trips',
+          tabBarIcon: ({ color, size }) => <FontAwesome5 name="bell" color={color} size={size} />,
+          headerShown: false,
+        }}
       />
-      <Tabs.Screen name="profilePage" options={{
-        tabBarLabel: 'Inbox',
-        tabBarIcon: ({ color, size }) => <Ionicons name="person-outline" color={color} size={size} />,
-      }}
+      <Tabs.Screen
+        name="profilePage"
+        options={{
+          tabBarLabel: 'Inbox',
+          tabBarIcon: ({ color, size }) => <Ionicons name="person-outline" color={color} size={size} />,
+          headerShown: false,
+        }}
       />
     </Tabs>
   )
--- app/patient/home/(tabs)/landingPage.tsx
+++ app/patient/home/(tabs)/landingPage.tsx
@@ -1,12 +1,115 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { ScrollView, View, Text, Image, TextInput, TouchableOpacity } from 'react-native'
+import React, { useState } from 'react'
+import tw from 'twrnc'
+import { Feather, FontAwesome } from '@expo/vector-icons';
+
+//Images
+import article1 from '@/assets/images/article1.png';
 
 const landingPage = () => {
-  return (
-    <View>
-      <Text>landingPage</Text>
-    </View>
-  )
+    const name = 'Ruchita';
+    const [searchQuery, setSearchQuery] = useState<string>('')
+    const articles = [
+        {
+            photo: article1,
+            title: 'The 25 Healthiest diet You Can Eat, According to drugs you are taking',
+            postedDate: 'Jul 10, 2023',
+            timeRead: 5
+        }
+    ]
+    return (
+        <View>
+            <View style={tw`flex-row pt-12 bg-[#D5ECF4]`}>
+                <View style={tw`w-1/2 px-5 gap-3 py-2`}>
+                    <View>
+                        <Image
+                            source={require('@/assets/images/home_profile.png')}
+                        />
+                    </View>
+                    <View>
+                        <Text style={tw`font-semibold text-xl`}>Welcome!</Text>
+                        <Text style={tw`text-xl text-gray-700 mb-2`}>{name}</Text>
+                        <Text>How is it going today?</Text>
+                    </View>
+                </View>
+                <View style={tw`w-1/2`}>
+                    <Image
+                        source={require('@/assets/images/home1.png')}
+                        style={tw`w-4/5`}
+                    />
+                </View>
+            </View>
+            <View style={tw`w-full rounded-full items-center `}>
+                <ScrollView style={tw`w-full`}>
+                    <View style={tw`items-center my-8 gap-7`}>
+                        <View style={tw`w-4/5 flex-row items-center border border-gray-300 rounded-full p-2`}>
+                            <Feather name="search" size={24} color="gray" style={tw`mr-2`} />
+                            <TextInput
+                                style={tw`flex-1 h-6 px-4`}
+                                placeholder="Search doctors, pharmacies or drugs"
+                                onChangeText={text => setSearchQuery(text)}
+                                value={searchQuery}
+                            />
+                        </View>
+                        <View style={tw`flex flex-row gap-12`}>
+                            <View style={tw`items-center`}>
+                                <TouchableOpacity style={tw`h-10 w-10 bg-blue-600 rounded-full items-center justify-center`}>
+                                    <FontAwesome name='stethoscope' size={22} color='white' />
+                                </TouchableOpacity>
+                                <Text style={tw`font-semibold`}>Top doctors</Text>
+                            </View>
+                            <View style={tw`items-center`}>
+                                <TouchableOpacity style={tw`h-10 w-10 bg-blue-600 rounded-full items-center justify-center`}>
+                                    <FontAwesome name='stethoscope' size={22} color='white' />
+                                </TouchableOpacity>
+                                <Text style={tw`font-semibold`}>Referral <br /> hospitals</Text>
+                            </View>
+                            <View style={tw`items-center`}>
+                                <TouchableOpacity style={tw`h-10 w-10 bg-blue-600 rounded-full items-center justify-center`}>
+                                    <FontAwesome name='stethoscope' size={22} color='white' />
+                                </TouchableOpacity>
+                                <Text style={tw`font-semibold`}>Pharmacy</Text>
+                            </View>
+                        </View>
+                    </View>
+                    <View style={tw`px-6`}>
+                        <View style={tw`flex-row justify-between `}>
+                            <Text style={tw`font-bold text-xl`}>Health article</Text>
+                            <TouchableOpacity>
+                                <Text style={tw`text-blue-600`}>See all</Text>
+                            </TouchableOpacity>
+                        </View>
+                        <View>
+                            {articles.map((article, i) => (
+                                <View
+                                    key={i}
+                                    style={tw`flex-row p-1 gap-2 border border-gray-400 rounded-md`}
+                                >
+                                    <View>
+                                        <Image
+                                            source={article.photo}
+                                        />
+                                    </View>
+                                    <View style={tw`gap-1`}>
+                                        <View style={tw`flex-row w-[70%] justify-between bg-red-500`}>
+                                            <View style={tw`w-4/5`}>
+                                                <Text style={tw``}>{article.title}</Text>
+                                            </View>
+                                            <FontAwesome name='bookmark-o' />
+                                        </View>
+                                        <View>
+                                            <Text>{article.postedDate}</Text>
+                                            <Text>{article.timeRead}min read</Text>
+                                        </View>
+                                    </View>
+                                </View>
+                            ))}
+                        </View>
+                    </View>
+                </ScrollView>
+            </View>
+        </View>
+    )
 }
 
 export default landingPage
